3386913d285e538340e4c2c25cff475277af1898
Kryštof Pilnáček <krystof.pilnacek@rossum.ai>
2021-01-19 14:06:09 +0100

    Dummy

--- src/common/constants.py
+++
@@ -1,1 +0,0 @@
-SERVICE_DESCRIPTION = "Service accepting PDF files containing one or more pages and rendering them to normalized png files."
--- src/common/enums.py
+++
@@ -1,8 +0,0 @@
-# Standard Library
-from enum import Enum
-
-
-class DocumentStatus(Enum):
-    IN_QUEUE = "IN_QUEUE"
-    PROCESSING = "PROCESSING"
-    DONE = "DONE"
